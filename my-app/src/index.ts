863068eaa0afb41db7e96d0f2c741fce5e145310
Rodevp <rodriguezrodrigor12@gmail.com>
2024-04-01 10:29:45 -0500

    metodos http y contexto

--- my-app/src/index.ts
+++ my-app/src/index.ts
@@ -7,6 +7,19 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
+app.get('/hello', (c) => {
+  c.header('X-message', 'Hi!')
+  return c.text('Hola mundito', 200)
+})
+
+app.post('/posts', (c) => c.text('Created!', 201))
+app.put('/posts/:id', (c) => c.text(`${c.req.param('id')} is updated!`, 200))
+
+app.patch('/posts/:id', (c) => c.text(`${c.req.param('id')} is partial updated!`, 200))
+
+app.delete('/posts/:id', (c) => c.text(`${c.req.param('id')} is deleted!`, 200))
+
+
 const port = 3000
 console.log(`Server is running on port ${port}`)
 
